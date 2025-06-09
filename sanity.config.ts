5167e1e3e927e37d67f69b0c9e05f89162c5b02c
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2025-06-09 14:01:38 -0300

    feat: adding workspace and import and export scripts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,18 +1,35 @@
+import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
 
-export default defineConfig({
-  name: 'default',
-  title: 'EventosAesas',
+export default defineConfig([
+  {
+    name: 'Production',
+    title: 'EventosAesas [Prod]',
+    basePath: '/eventos-aesas-production',
 
-  projectId: 'ejmsm84f',
-  dataset: 'production',
+    projectId: 'ejmsm84f',
+    dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+    plugins: [structureTool(), visionTool()],
 
-  schema: {
-    types: schemaTypes,
+    schema: {
+      types: schemaTypes,
+    },
   },
-})
+  {
+    name: 'Develop',
+    title: 'EventosAesas [Dev]',
+    basePath: '/eventos-aesas-dev',
+
+    projectId: 'ejmsm84f',
+    dataset: 'develop',
+
+    plugins: [structureTool(), visionTool()],
+
+    schema: {
+      types: schemaTypes,
+    },
+  },
+])
