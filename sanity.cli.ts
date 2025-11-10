4c7c526a4a89c19094deca11caef4891db15644c
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2025-11-10 10:31:23 -0300

    add programatic content fields on event

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -5,6 +5,9 @@ export default defineCliConfig({
     projectId: 'vq1wc9fu',
     dataset: 'production'
   },
+  deployment: {
+    appId: 'ufs415eqciyv56ep7zniosgk',
+  },
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
